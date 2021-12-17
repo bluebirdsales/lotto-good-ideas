f180f8a03ecda251ac43b32c051e58a836560bc6
stephen <stephen.stocking@gmail.com>
2021-12-17 00:29:35 -0500

    added environment vars

--- src/firebase/firebase_config.js
+++ src/firebase/firebase_config.js
@@ -8,13 +8,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCRzcrBtSg4KNjdHbBy_pPMXQDBJUHf_GI",
-    authDomain: "lotto-app-40d25.firebaseapp.com",
-    projectId: "lotto-app-40d25",
-    storageBucket: "lotto-app-40d25.appspot.com",
-    messagingSenderId: "758249634328",
-    appId: "1:758249634328:web:a7d52180124b9b9c90216d",
-    measurementId: "G-6KGRSK5NSC",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_API_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
